de25ee8bc13d9a66fa5c3c5264153237fcf3c16e
pratyushchauhan147 <pratyushchauhan147code@gmail.com>
2025-01-06 17:25:27 +0530

    Comple

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.set("view engine","ejs")
+app.set("views", path.join(__dirname, "views"));
 app.use(cookieParser())
 app.use(expressSession({
     resave:false,
