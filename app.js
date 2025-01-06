8590ee721bf402634c4b38568793345e944024c0
pratyushchauhan147 <pratyushchauhan147code@gmail.com>
2025-01-06 16:39:42 +0530

    Completed

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.set("view engine","ejs")
 app.use(cookieParser())
 app.use(expressSession({
     resave:false,
@@ -27,7 +28,7 @@ app.use(expressSession({
     secret:process.env.EXPRESS_SESSION_KEY
 }))
 app.use(flash())
-app.set("view engine","ejs")
+
 
 
 
